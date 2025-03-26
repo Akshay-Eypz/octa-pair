3cb9f911142dd033f78bfffc257778077161edf1
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-03-27 03:09:34 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,7 +57,7 @@ router.get('/', async (req, res) => {
 
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
 
-                        const sid = string_session;
+                        const sid = `izumi~${string_session}`;
 
                         const dt = await EypzPairWeb.sendMessage(user_jid, {
                             text: sid
