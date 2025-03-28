b0e98808f6e81f3be0bfaff096e7bbd5ea77ea00
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-03-28 16:56:35 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'sataniceypz@gmail.com',
-    password: 'eypzgod666',
+    email: 'octa@onlyfans.com',
+    password: 'your mega pass word',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
